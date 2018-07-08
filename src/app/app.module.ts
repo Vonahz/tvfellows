c3e1e4101fa1c4271c211bf947a551ec6c3e4d5e
Milen Karmidzhanov <karmidzhanov@gmail.com>
2018-07-08 12:44:19 +0300

    Adding basic stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './views/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatProgressSpinnerModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
