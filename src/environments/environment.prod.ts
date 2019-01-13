ed8899b14cc4e08a77847fa79dce0a42cdd0ba9f
Milen Karmidzhanov <karmidzhanov@gmail.com>
2019-01-13 23:10:01 +0200

    Fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.themoviedb.org/3/tv',
   apiKey: '6917fecb06f478c815ac557c6dbeed65',
-  imagesUrl: 'https://image.tmdb.org/t/p',
+  postersUrl: 'https://image.tmdb.org/t/p',
 };
